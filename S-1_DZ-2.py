aae77080ff20425f64250868f59330cf8cb0f5d0
Oksana <caps14@gmail.com>
2022-07-15 22:45:04 +0800

    git add S-1_DZ-2,3,4.py

---
+++ S-1_DZ-2.py
@@ -0,0 +1,23 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. 
+
+def Function(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def Predicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = Function(3)
+
+if Predicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
